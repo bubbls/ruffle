9e68d787835e945c78964b5145e43d02e9a00cd5
bubbls <burner98382@gmail.com>
2025-01-05 13:33:22 -0700

    vex 5

---
+++ vex5bobydobpoo.js
@@ -0,0 +1,17 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+version = "6";
+libs = ["https://web.archive.org/web/20241223001333/https://cdn.jsdelivr.net/gh/CFiltered/Matt@50f9c1bd0d376b40cb8545519b147a4030598724/other/vex4/cdn.jsdelivr.net/npm/%40orange-games/phaser-ads%402.2/build/phaser-ads.min.js", "https://web.archive.org/web/20241223001333/https://cdn.jsdelivr.net/npm/@orange-games/phaser-cachebuster@2.0/build/phaser-cachebuster.min.js", "https://web.archive.org/web/20241223001333/https://cdn.jsdelivr.net/npm/@orange-games/phaser-input@2.0/build/phaser-input.min.js", "https://web.archive.org/web/20241223001333/https://cdn.jsdelivr.net/npm/@orange-games/phaser-nineslice@2.0/build/phaser-nineslice.min.js", "https://web.archive.org/web/20241223001333/https://cdn.jsdelivr.net/npm/@orange-games/phaser-spine@3.0/build/phaser-spine.min.js", "https://web.archive.org/web/20241223001333/https://cdn.jsdelivr.net/npm/@orange-games/phaser-super-storage@1.0/build/phaser-super-storage.min.js", "https://web.archive.org/web/20241223001333/https://cdn.jsdelivr.net/npm/@orange-games/splash@3.5/build/splash.min.js"];
+
+
+}
